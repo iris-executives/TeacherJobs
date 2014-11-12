710a11dd8ac553d0451394fc5388abaf20a392e5
shams <shams@questionemploi.com>
2014-11-12 19:48:10 +0400

    Change 'Pobox' to 'pobox'

--- app/TJ/Entities/Employer.php
+++ app/TJ/Entities/Employer.php
@@ -7,34 +7,18 @@ use Eloquent;
 
 class Employer extends Eloquent {
 
-	protected $table = 'employers';
+    protected $table = 'employers';
     protected $employer;
     public $timestamps = true;
 
     use SoftDeletingTrait;
 
     protected $dates = ['deleted_at'];
-    protected $fillable = array('name', 'description', 'phone', 'address', 'city', 'Pobox', 'logo');
-    protected $visible = array('name', 'description', 'phone', 'address', 'city', 'Pobox', 'logo');
+    protected $fillable = array('name', 'description', 'phone', 'address', 'city', 'pobox', 'logo');
+    protected $visible = array('name', 'description', 'phone', 'address', 'city', 'pobox', 'logo');
 
     public function members()
     {
             return $this->hasMany('EmployerMember');
     }
-
-    public function create($name, $description, $phone, $address, $Pobox, $logo)
-    {
-            return $this->employer->create(compact('name', 'description','phone','address','Pobox','logo'));
-    }
-
-    public function update($id, $name, $description, $phone, $address, $Pobox, $logo)
-    {
-        return $this->employer->where($this->employer->getKeyName(), $id)->update(compact('name', 'description','phone','address','Pobox','logo'));
-    }
-    
-    public function delete($id)
-    {
-        return $this->employer->where($this->employer->getKeyName(), $id)->delete();
-    }
-
 }

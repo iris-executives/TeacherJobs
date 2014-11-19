cfdf00ad32863133dea80f2e2b22095134f53938
shams <shams@questionemploi.com>
2014-11-19 19:02:47 +0400

    implement applyToJob() method

--- app/TJ/Entities/JobSeeker.php
+++ app/TJ/Entities/JobSeeker.php
@@ -14,8 +14,8 @@ class JobSeeker extends Eloquent {
 	use SoftDeletingTrait;
 
 	protected $dates = ['deleted_at'];
-	protected $fillable = array('residence', 'city_live_in', 'city_work_in', 'mobile1', 'mobile2', 'dob', 'primary_language', 'secondary_language', 'will_relocate', 'is_anonyous', 'picture', 'employment_status', 'work_experience', 'sector_most_experience', 'other_sector1', 'other_sector2', 'awards', 'achievements', 'recognitions', 'education_level', 'graduation_date', 'ongoing_education', 'graduation_date2', 'pro_certifications', 'ongoing_certifications', 'has_managed_others', 'notice_period', 'is_registered', 'is_subscribed_newsletter');
-	protected $visible = array('residence', 'city_live_in', 'city_work_in', 'mobile1', 'mobile2', 'dob', 'primary_language', 'secondary_language', 'will_relocate', 'is_anonyous', 'picture', 'employment_status', 'work_experience', 'sector_most_experience', 'other_sector1', 'other_sector2', 'awards', 'achievements', 'recognitions', 'education_level', 'graduation_date', 'ongoing_education', 'graduation_date2', 'pro_certifications', 'ongoing_certifications', 'has_managed_others', 'notice_period', 'is_registered', 'is_subscribed_newsletter');
+	protected $fillable = array('user_id', 'gender', 'residence', 'city_live_in', 'city_work_in', 'mobile1', 'mobile2', 'dob', 'primary_language', 'secondary_language', 'will_relocate', 'is_anonyous', 'picture', 'employment_status', 'work_experience', 'sector_most_experience', 'other_sector1', 'other_sector2', 'awards', 'achievements', 'recognitions', 'education_level', 'graduation_date', 'ongoing_education', 'graduation_date2', 'pro_certifications', 'ongoing_certifications', 'has_managed_others', 'notice_period', 'is_registered', 'is_subscribed_newsletter', 'country_id');
+	protected $visible = array('user_id', 'gender', 'residence', 'city_live_in', 'city_work_in', 'mobile1', 'mobile2', 'dob', 'primary_language', 'secondary_language', 'will_relocate', 'is_anonyous', 'picture', 'employment_status', 'work_experience', 'sector_most_experience', 'other_sector1', 'other_sector2', 'awards', 'achievements', 'recognitions', 'education_level', 'graduation_date', 'ongoing_education', 'graduation_date2', 'pro_certifications', 'ongoing_certifications', 'has_managed_others', 'notice_period', 'is_registered', 'is_subscribed_newsletter', 'country_id');
 
 	public function user()
 	{
@@ -32,9 +32,13 @@ class JobSeeker extends Eloquent {
 		return $this->hasMany('TJ\Entities\JobApplication');
 	}
         
-        public function applyToJob()
+        public function applyToJob($job_post_id)
         {
-                //
+                //create a job application
+                $job_application = JobApplication::create([ "job_post_id" => $job_post_id,
+                                                            "jobseeker_id" => $this->id ]);
+                
+                return $job_application;
         }        
 
 }
